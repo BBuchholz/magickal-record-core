e435c532d32affc32b97db1e83f1c08f0d1d1720
ENTOMDT <entomdt@gmail.com>
2021-02-02 18:25:13 +0000

    Add TDD tests for Record Entries

--- test/magickal-record.js
+++ test/magickal-record.js
@@ -3,44 +3,56 @@ var magickalRecord = require("../app/magickal-record");
 
 describe("Magickal Record", function() {
 
+    const myMagickalRecord = new magickalRecord();
+    var testEntry;
+
+    beforeEach(function(){
+        myMagickalRecord.clearAllEntries();
+        testEntry = myMagickalRecord.createEntry("A test entry");
+    });
+
     describe("Record Entries", function() {
 
         it("should have an entry UUID", function(){
 
-            expect(true).to.equal(false);
+            expect(testEntry).to.have.property("uuid");
+            
         });
 
         it("should return a list of parent records", function(){
 
-            expect(true).to.equal(false);
+            expect(testEntry).to.have.property("parentRecords");
+            
         });
 
         it("should have a source UUID", function(){
             // The source of this entry (a secondary source 
             // if it’s copied from somewhere, otherwise the
             // current user, if signed into the Magickal record)
-            expect(true).to.equal(false);
+            
+            expect(testEntry).to.have.property("sourceUuid");
         });
 
         it("should have an entry title", function(){
 
-            expect(true).to.equal(false);
+            expect(testEntry).to.have.property("title");
         });
 
         it("should have an entry body", function(){
 
-            expect(true).to.equal(false);
+            expect(testEntry).to.have.property("body");
         });
 
         it("should return an entered-on timestamp", function(){
 
             //make this a tag
-            expect(true).to.equal(false);
+            
+            expect(testEntry).to.have.property("enteredOn");
         });
 
         it("should return a list of Wxrd Tags", function(){
 
-            expect(true).to.equal(false);
+            expect(testEntry).to.have.property("wxrdTags");
         });
 
     });
@@ -49,17 +61,20 @@ describe("Magickal Record", function() {
 
         it("should have a Wxrd UUID", function(){
 
-            expect(true).to.equal(false);
+            //TODO fill in test
+            
         });
 
         it("should return an ordered list of aliases", function(){
 
-            expect(true).to.equal(false);
+            //TODO fill in test
+            
         });
 
         it("should return a metadata set", function(){
 
-            expect(true).to.equal(false);
+            //TODO fill in test
+            
         });
     });
 
@@ -67,7 +82,8 @@ describe("Magickal Record", function() {
 
         it("should have a name for the Source Identity", function(){
 
-            expect(true).to.equal(false);
+            
+            //TODO fill in test
         });
 
         it("should have a source UUID", function(){
@@ -75,18 +91,21 @@ describe("Magickal Record", function() {
             // uuid to use when using this identity as a primary 
             // source (ie, all entries that are original ideas from
             // this source identity)
-            expect(true).to.equal(false);
+            
+            //TODO fill in test
         });
         
         it("should have an optionally associated email", function(){
 
-            expect(true).to.equal(false);
+            
+            //TODO fill in test
         });
         
         it("should have an optionally associated identity hash as a signature verification", function(){
 
             //combine email, identity name, a salt, and a passcode to generate hash
-            expect(true).to.equal(false);
+            
+            //TODO fill in test
         });
         
     });
@@ -95,22 +114,26 @@ describe("Magickal Record", function() {
 
         it("should return a list of expected parameters, both optional and required", function() {
 
-            expect(true).to.equal(false);
+            //TODO fill in test
+            
         });
 
         it("should return an operation result", function() {
 
-            expect(true).to.equal(false);
+            //TODO fill in test
+            
         });
 
         it("should have a success status in operation result", function() {
 
-            expect(true).to.equal(false);
+            //TODO fill in test
+            
         });
 
         it("should provide messages with the operation result", function() {
 
-            expect(true).to.equal(false);
+            //TODO fill in test
+            
         });
 
     });
@@ -118,76 +141,79 @@ describe("Magickal Record", function() {
     describe("Record Browser", function() {
         
         it("should include basic meta operations in the operations list", function(){
-            throw new Error("Test Not Yet Implemented");
+            
+            //TODO fill in test
         }); 
 
         it("should be in test mode", function(){
         
-            var testMode = magickalRecord.isInTestingMode();
+            var testMode = myMagickalRecord.isInTestingMode();
         
             expect(testMode).to.equal(true);
         
         });
 
-        it("should list starting record entries as zero", function(){
+        it("should return the created entry when creating new entry", function(){
 
-            expect(magickalRecord.recordEntriesCount()).to.equal(0);
-        });
-
-        it("should return uuid when creating new entry", function(){
-
-            var newUuid = magickalRecord.createEntry("this is an entry");
-            var isValid = false; //replace with regular expression validation check
-            expect(isValid).to.equal(true);
+            var contentToCreate = "this is a new entry";
+            var newEntry = myMagickalRecord.createEntry(contentToCreate);
+            
+            expect(newEntry.body).to.equal(contentToCreate);
         });
 
         it("should return previously created entry when passed generated uuid", function(){
 
-            var testEntry = undefined;
-            var newUuid = magickalRecord.createEntry(testEntry.content);
-
-            expect(testEntry).to.equal(magickalRecord.getEntryByUuid(newUuid));
+            var newlyCreatedEntry = myMagickalRecord.createEntry(testEntry.content);
+            var retrievedEntry = myMagickalRecord.getEntryByUuid(newlyCreatedEntry.uuid);
+            expect(testEntry.content).to.equal(retrievedEntry.content);
         });
 
         it("should get sources list", function() {
 
             //Include in "Rogue Scholar" flippable feature (though still part of 0.3.0, just something that can be turned on or off, for users not planning on using any external sources, ie, just using it as a personal log)
-            expect(true).to.equal(false);
+            
+            //TODO fill in test
         });
 
         it("should list flippable features", function() {
 
-            expect(true).to.equal(false);
+            //TODO fill in test
         });
         
         it("should toggle flippable features by supplying feature name", function() {
 
-            expect(true).to.equal(false);
+            
+            //TODO fill in test
         });
         
         it("should report if flippable feature is active", function() {
 
-            expect(true).to.equal(false);
+            //TODO fill in test
+            
         });
         
         it("should list all Wxrds", function() {
 
-            expect(true).to.equal(false);
+            //TODO fill in test
+            
         });
         
         it("should get all entries for a given time span", function() {
 
-            expect(true).to.equal(false);
+            //TODO fill in test
+            
         });
         
         it("should get all entries having a given set of Wxrd tags", function() {
 
-            expect(true).to.equal(false);
+            //TODO fill in test
+            
         });
         
         it("should get all child entries of a given entry", function() {
 
-            expect(true).to.equal(false);
+            //TODO fill in test
+            
         });
                 
     });
